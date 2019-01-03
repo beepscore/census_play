100702a47b1f8569cce2858fc9c8ae6d8d81433c
beepscore <support@beepscore.com>
2019-01-02 18:24:17 -0800

    Add instance variable "last" and method populate_last

--- names.py
+++ names.py
@@ -13,17 +13,29 @@ class Names:
     # https://stackoverflow.com/questions/7571635/fastest-way-to-check-if-a-value-exist-in-a-list
     # alternatively could search a pandas dataframe or series
     first_female = {}
+    last = {}
 
     def populate_first_female(self):
         filename = './data/names/dist.female.first'
 
         # use pandas to get data
-        df_first = pd.read_csv(filename, sep=' ')
-        series = df_first.iloc[:, 0]
+        df = pd.read_csv(filename, sep=' ')
+        series = df.iloc[:, 0]
         series.dropna(inplace=True)
 
         # convert pandas series to set, store in instance variable
         self.first_female = set(series)
+
+    def populate_last(self):
+        filename = './data/names/Names_2010Census_Top1000.csv'
+
+        # use pandas to get data
+        df = pd.read_csv(filename)
+        series = df.iloc[:, 0]
+        series.dropna(inplace=True)
+
+        # convert pandas series to set, store in instance variable
+        self.last = set(series)
 
 
 if __name__ == "__main__":
@@ -31,6 +43,10 @@ if __name__ == "__main__":
     names = Names()
     names.populate_first_female()
     print(names.first_female)
+    names.populate_last()
+    print(names.last)
 
     if 'Kathy'.upper() in names.first_female:
         print('found Kathy')
+    if 'little'.upper() in names.last:
+        print('found little')
